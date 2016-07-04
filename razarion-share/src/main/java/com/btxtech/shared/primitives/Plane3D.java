6c9c45774560a52f892b1c4f3cd6b9e7bd71125f
Beat Keller <beat.keller@btxtech.com>
2016-07-05 00:50:48 +0200

    Shadow bugfix

--- razarion-share/src/main/java/com/btxtech/shared/primitives/Plane3D.java
+++ razarion-share/src/main/java/com/btxtech/shared/primitives/Plane3D.java
@@ -1,5 +1,8 @@
 package com.btxtech.shared.primitives;
 
+import com.btxtech.game.jsre.client.common.DecimalPosition;
+import com.btxtech.game.jsre.common.MathHelper;
+
 /**
  * Created by Beat
  * 03.06.2016.
@@ -9,6 +12,9 @@ public class Plane3d {
     private double b;
     private double c;
     private double d;
+    private Vertex origin;
+    private Vertex planeXAxis;
+    private Vertex planeYAxis;
 
     public Plane3d(Vertex norm, Vertex point) {
         a = norm.getX();
@@ -24,5 +30,34 @@ public class Plane3d {
     public Vertex project(Vertex point) {
         double f = (-a * point.getX() - b * point.getY() - c * point.getZ() - d);
         return point.add(getNorm().multiply(f));
+    }
+
+    public void setOptionalOrigin(Vertex originToProject, Vertex planeXAxis, Vertex planeYAxis) {
+        if (!MathHelper.compareWithPrecision(planeXAxis.dot(getNorm()), 0.0)) {
+            throw new IllegalArgumentException("X Axis is not perpendicular to norm");
+        }
+        if (!MathHelper.compareWithPrecision(planeYAxis.dot(getNorm()), 0.0)) {
+            throw new IllegalArgumentException("Y Axis is not perpendicular to norm");
+        }
+        if (!MathHelper.compareWithPrecision(planeXAxis.dot(planeYAxis), 0.0)) {
+            throw new IllegalArgumentException("X Axis and Y Axis are not perpendicular");
+        }
+        origin = project(originToProject);
+        this.planeXAxis = planeXAxis;
+        this.planeYAxis = planeYAxis;
+    }
+
+    public DecimalPosition getPlaneCoordinates(Vertex vertex) {
+        if (origin == null || planeXAxis == null || planeYAxis == null) {
+            throw new IllegalStateException("origin == null || planeXAxis == null || planeYAxis == null");
+        }
+        if (!MathHelper.compareWithPrecision(vertex.distance(project(vertex)), 0.0)) {
+            throw new IllegalArgumentException("Point is not on plane");
+        }
+        Vertex relative = vertex.sub(origin);
+        double x = planeXAxis.dot(relative);
+        double y = planeYAxis.dot(relative);
+
+        return new DecimalPosition(x, y);
     }
 }
