60e1706ad4335c9bd061b008a304074e8bc8000c
Beat Keller <beat.keller@btxtech.com>
2017-02-03 14:52:44 +0100

    Particle added (part 2)

--- razarion-client/src/main/java/com/btxtech/client/renderer/shaders/Particle.frag
+++ razarion-client/src/main/java/com/btxtech/client/renderer/shaders/Particle.frag
@@ -4,5 +4,5 @@ uniform vec4 uColor;
 uniform float uProgress;
 
 void main(void) {
-   gl_FragColor = vec4(0.5, 0.5, 0.5, uProgress);
+   gl_FragColor = vec4(0.2, 0.2, 0.2, uProgress);
 }
--- razarion-ui-service/src/main/java/com/btxtech/uiservice/particle/ParticleService.java
+++ razarion-ui-service/src/main/java/com/btxtech/uiservice/particle/ParticleService.java
@@ -15,16 +15,19 @@ import java.util.List;
 @ApplicationScoped
 public class ParticleService {
     private static final int GENERATION_DELAY = 100;
+    private static final int GENERATION_COUNT = 50;
+    private static final int GENERATION_RANDOM_DISTANCE = 5;
     private List<Particle> particles = new ArrayList<>();
     private long lastGenerationTime;
 
     public List<ModelMatrices> provideModelMatrices(long timestamp) {
         List<ModelMatrices> modelMatricesList = new ArrayList<>();
         if (lastGenerationTime + GENERATION_DELAY < timestamp) {
-            particles.add(new Particle(new Vertex(200, 200, 2), timestamp));
-            particles.add(new Particle(new Vertex(200 + Particle.HALF_EDGE, 200, 2), timestamp));
-            particles.add(new Particle(new Vertex(200, 200 + Particle.HALF_EDGE, 2), timestamp));
-            particles.add(new Particle(new Vertex(200 + Particle.HALF_EDGE, 200 + Particle.HALF_EDGE, 2), timestamp));
+            for (int i = 0; i < GENERATION_COUNT; i++) {
+                double xRand = Math.random() * GENERATION_RANDOM_DISTANCE;
+                double yRand = Math.random() * GENERATION_RANDOM_DISTANCE;
+                particles.add(new Particle(new Vertex(200 + xRand, 200 + yRand, 2), timestamp));
+            }
             lastGenerationTime = timestamp;
         }
         for (Iterator<Particle> iterator = particles.iterator(); iterator.hasNext(); ) {
