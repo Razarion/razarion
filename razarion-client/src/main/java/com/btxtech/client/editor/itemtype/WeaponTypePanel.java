9006217b16517fd01e9341f7fa7c81afb284b41e
Beat Keller <beat.keller@btxtech.com>
2017-09-08 12:39:39 +0200

    WeaponType editor added

--- razarion-client/src/main/java/com/btxtech/client/editor/itemtype/WeaponTypePanel.java
+++ razarion-client/src/main/java/com/btxtech/client/editor/itemtype/WeaponTypePanel.java
@@ -1,13 +1,21 @@
 package com.btxtech.client.editor.itemtype;
 
+import com.btxtech.client.editor.widgets.childpanel.ChildContainer;
+import com.btxtech.client.editor.widgets.itemtype.baselist.BaseItemTypeListWidget;
+import com.btxtech.client.editor.widgets.shape3dwidget.Shape3DReferenceFiled;
+import com.btxtech.shared.gameengine.datatypes.itemtype.TurretType;
 import com.btxtech.shared.gameengine.datatypes.itemtype.WeaponType;
 import com.google.gwt.user.client.TakesValue;
 import com.google.gwt.user.client.ui.Composite;
+import org.jboss.errai.common.client.dom.NumberInput;
 import org.jboss.errai.databinding.client.api.DataBinder;
 import org.jboss.errai.ui.shared.api.annotations.AutoBound;
+import org.jboss.errai.ui.shared.api.annotations.Bound;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Created by Beat
@@ -18,10 +26,50 @@ public class WeaponTypePanel extends Composite implements TakesValue<WeaponType>
     @Inject
     @AutoBound
     private DataBinder<WeaponType> dataBinder;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput range;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput damage;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput detonationRadius;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput reloadTime;
+    @Inject
+    @DataField
+    private BaseItemTypeListWidget disallowedItemTypes;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput projectileSpeed;
+    @Inject
+    @DataField
+    private Shape3DReferenceFiled projectileShape3DId;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput muzzleFlashParticleConfigId;
+    @Inject
+    @Bound
+    @DataField
+    private NumberInput detonationParticleConfigId;
+    @Inject
+    @DataField
+    private ChildContainer<TurretType> turretType;
 
     @Override
     public void setValue(WeaponType weaponType) {
         dataBinder.setModel(weaponType);
+        disallowedItemTypes.init(weaponType.getDisallowedItemTypes(), weaponType::setDisallowedItemTypes);
+        turretType.init(weaponType.getTurretType(), weaponType::setTurretType, TurretType::new, TurretTypePanel.class);
+        projectileShape3DId.init(weaponType.getProjectileShape3DId(), weaponType::setProjectileShape3DId);
     }
 
     @Override
