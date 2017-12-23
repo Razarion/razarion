3881e5a6ec8c5478e4ca6b3c06c8788df276519f
Beat Keller <beat.keller@btxtech.com>
2017-12-23 01:57:40 +0100

    Improvements

--- razarion-client/src/main/java/com/btxtech/client/dialog/unlock/UnlockItemWidget.java
+++ razarion-client/src/main/java/com/btxtech/client/dialog/unlock/UnlockItemWidget.java
@@ -1,6 +1,9 @@
 package com.btxtech.client.dialog.unlock;
 
 import com.btxtech.client.dialog.framework.ClientModalDialogManagerImpl;
+import com.btxtech.shared.gameengine.ItemTypeService;
+import com.btxtech.shared.gameengine.datatypes.itemtype.BaseItemType;
+import com.btxtech.shared.rest.RestUrl;
 import com.btxtech.uiservice.i18n.I18nHelper;
 import com.btxtech.uiservice.unlock.UnlockUiService;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -10,6 +13,7 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import org.jboss.errai.common.client.api.IsElement;
 import org.jboss.errai.common.client.dom.HTMLElement;
+import org.jboss.errai.common.client.dom.Span;
 import org.jboss.errai.common.client.dom.TableRow;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.EventHandler;
@@ -27,6 +31,8 @@ public class UnlockItemWidget implements TakesValue<UnlockItemModel>, IsElement 
     @Inject
     private UnlockUiService unlockUiService;
     @Inject
+    private ItemTypeService itemTypeService;
+    @Inject
     private ClientModalDialogManagerImpl modalDialogManager;
     @Inject
     @DataField
@@ -35,6 +41,9 @@ public class UnlockItemWidget implements TakesValue<UnlockItemModel>, IsElement 
     @Inject
     @DataField
     private Image image;
+    @Inject
+    @DataField
+    private Span span;
     @Inject
     @DataField
     private Label unlockTitle;
@@ -55,7 +64,13 @@ public class UnlockItemWidget implements TakesValue<UnlockItemModel>, IsElement 
     @Override
     public void setValue(UnlockItemModel unlockItemModel) {
         this.unlockItemModel = unlockItemModel;
-        image.setUrl(unlockItemModel.getImageUrl());
+        if (unlockItemModel.getLevelUnlockConfig().getBaseItemType() != null) {
+            BaseItemType baseItemType = itemTypeService.getBaseItemType(unlockItemModel.getLevelUnlockConfig().getBaseItemType());
+            image.setUrl(RestUrl.getImageServiceUrlSafe(baseItemType.getThumbnail()));
+            span.setTextContent("+" + unlockItemModel.getLevelUnlockConfig().getBaseItemTypeCount());
+        } else {
+            throw new UnsupportedOperationException("UnlockItemWidget.setValue() unlockItemModel.getLevelUnlockConfig().getBaseItemType() == null ...TODO...");
+        }
         unlockTitle.setText(I18nHelper.getLocalizedString(unlockItemModel.getLevelUnlockConfig().getI18nName()));
         unlockDescription.setText(I18nHelper.getLocalizedString(unlockItemModel.getLevelUnlockConfig().getI18nDescription()));
         unlockCrystal.setText(I18nHelper.getConstants().unlockCrystalCost(unlockItemModel.getLevelUnlockConfig().getCrystalCost()));
@@ -76,7 +91,7 @@ public class UnlockItemWidget implements TakesValue<UnlockItemModel>, IsElement 
     private void onUnlockButtonClicked(ClickEvent event) {
         unlockUiService.unlockViaCrystal(unlockItemModel.getLevelUnlockConfig(), success -> {
             if (success) {
-                if(!unlockUiService.hasItems2Unlock()) {
+                if (!unlockUiService.hasItems2Unlock()) {
                     unlockItemModel.closeDialog();
                 }
             } else {
