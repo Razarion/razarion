722e1b08b024f3019b7bde08ead6dd2e7940b96a
Beat <beat.keller@btxtech.com>
2020-02-20 20:46:56 +0100

    [CRUD] + [GUI editor] pretty close

--- razarion-server/src/test/java/com/btxtech/server/systemtests/GroundEditorControllerTest.java
+++ razarion-server/src/test/java/com/btxtech/server/systemtests/GroundEditorControllerTest.java
@@ -3,8 +3,6 @@ package com.btxtech.server.systemtests;
 import com.btxtech.shared.dto.GroundConfig;
 import com.btxtech.shared.dto.ObjectNameId;
 import com.btxtech.shared.rest.GroundEditorController;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -32,27 +30,28 @@ public class GroundEditorControllerTest extends EmptyDockerContainer {
     @Test
     public void groundGround() {
         login("admin@admin.com", "1234");
-        ObjectMapper mapper = new ObjectMapper();
 
         List<ObjectNameId> objectNameIds = groundEditorController.getObjectNameIds();
         Assert.assertEquals(0, objectNameIds.size());
-        GroundConfig groundConfig1 = mapper.convertValue(groundEditorController.create(), GroundConfig.class);
+        GroundConfig groundConfig1 = groundEditorController.create();
 
         objectNameIds = groundEditorController.getObjectNameIds();
         Assert.assertEquals(1, objectNameIds.size());
         Assert.assertEquals(objectNameIds.get(0).getId(), groundConfig1.getId());
 
-        GroundConfig groundConfig2 = mapper.convertValue(groundEditorController.read(groundConfig1.getId()), GroundConfig.class);
+        GroundConfig groundConfig2 = groundEditorController.read(groundConfig1.getId());
         assertThat(groundConfig2, is(samePropertyValuesAs(groundConfig1)));
+        groundConfig2.setInternalName("Internal 2");
+        groundEditorController.update(groundConfig2);
+        GroundConfig groundConfig3 = groundEditorController.read(groundConfig1.getId());
+        assertThat(groundConfig3, is(samePropertyValuesAs(groundConfig2)));
 
-        JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, GroundConfig.class);
-        List<GroundConfig> groundConfigs = mapper.convertValue(groundEditorController.read(), type);
+        List<GroundConfig> groundConfigs = groundEditorController.read();
         Assert.assertEquals(1, groundConfigs.size());
-        assertThat(groundConfigs.get(0), is(samePropertyValuesAs(groundConfig1)));
+        assertThat(groundConfigs.get(0), is(samePropertyValuesAs(groundConfig3)));
 
-        groundEditorController.delete(groundConfig1.getId());
+        groundEditorController.delete(groundConfig3.getId());
         Assert.assertEquals(0, groundEditorController.getObjectNameIds().size());
-
     }
 
 }
--- razarion-share/src/main/java/com/btxtech/shared/dto/ObjectNameIdProvider.java
+++ razarion-share/src/main/java/com/btxtech/shared/dto/ObjectNameIdProvider.java
@@ -1,9 +1,14 @@
 package com.btxtech.shared.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /**
  * Created by Beat
  * 23.08.2016.
  */
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.CLASS,
+        property = "type")
 public interface ObjectNameIdProvider {
     ObjectNameId createObjectNameId();
 }
