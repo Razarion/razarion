210c7fedebab2c6a3051d1234ce7e3166c6ed7d8
Beat <beat.keller@btxtech.com>
2020-03-08 16:07:12 +0100

    [SYSTEM TESTS] Improved

--- razarion-server/src/test/java/com/btxtech/server/ClientArquillianBaseTest.java
+++ razarion-server/src/test/java/com/btxtech/server/ClientArquillianBaseTest.java
@@ -8,10 +8,6 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
@@ -19,7 +15,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.client.ClientResponseFilter;
-import java.io.File;
 import java.util.List;
 
 /**
@@ -35,28 +30,7 @@ public class ClientArquillianBaseTest {
 
     @Deployment
     public static Archive<?> createDeployment() {
-        try {
-            // Do not ad weld-core dependency for deproxy
-            File[] libraries = Maven.resolver().loadPomFromFile("./pom.xml").importRuntimeDependencies().resolve("org.unitils:unitils-core:4.0-SNAPSHOT", "org.easymock:easymock:3.4", "org.subethamail:subethasmtp:3.1.7").withTransitivity().asFile();
-
-            WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "test.war")
-                    .addPackages(true, "com.btxtech.server")
-                    //.as(ExplodedImporter.class).importDirectory((new File("./target/classes"))).as(WebArchive.class)
-                    //.as(ExplodedImporter.class).importDirectory((new File("../razarion-share/target/classes"))).as(WebArchive.class)
-                    .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                    .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                    .addAsResource("mongodb/PlanetBackup.json", "mongodb/PlanetBackup.json")
-                    .addAsResource("mongodb/ServerItemTracking.json", "mongodb/ServerItemTracking.json")
-                    .addAsResource("templates", "templates")
-                    .addAsResource("Razarion.properties", "Razarion.properties")
-                    .addAsResource("Razarion_de.properties", "Razarion_de.properties")
-                    .addAsLibraries(libraries);
-            System.out.println(webArchive.toString(true));
-            return webArchive;
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-            throw throwable;
-        }
+        return null;
     }
 
     protected String getRestUrl() {
