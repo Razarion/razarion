5cdffc690660a1f05dd6dcd73867764aa758f7af
Beat <beat.keller@btxtech.com>
2020-03-18 23:16:30 +0100

    Improvements

--- razarion-server/src/test/java/com/btxtech/server/sql/users.sql
+++ razarion-server/src/test/java/com/btxtech/server/sql/users.sql
@@ -1,11 +1,11 @@
 -- Create admin user (admin@admin.com, pwd: 1234)
-INSERT INTO USER (id, email, passwordHash, verificationDoneDate, admin, crystals, xp)
-VALUES (1, 'admin@admin.com',
+INSERT INTO USER (email, passwordHash, verificationDoneDate, admin, crystals, xp)
+VALUES ('admin@admin.com',
         'qKfYO+K4nrC4UZwdquWOMHoOYFw7qNPkhOBR9Df1iCbD+YcPX2ofbNg3H3zHJ+HzXz32oQwYQUC7/K/tP1nAvg==',
         '2020-01-27 20:00:00', true, 0, 0);
 
 -- Create user (user@user.com, pwd: 1234)
-INSERT INTO USER (id, email, passwordHash, verificationDoneDate, admin, crystals, xp)
-VALUES (2, 'user@user.com',
+INSERT INTO USER (email, passwordHash, verificationDoneDate, admin, crystals, xp)
+VALUES ('user@user.com',
         'qKfYO+K4nrC4UZwdquWOMHoOYFw7qNPkhOBR9Df1iCbD+YcPX2ofbNg3H3zHJ+HzXz32oQwYQUC7/K/tP1nAvg==',
         '2020-01-27 20:00:00', false, 0, 0);
