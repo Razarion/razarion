bcec7b5a844e903f773d7dcb2d120884ec6cf311
Beat <beat.keller@btxtech.com>
2020-03-24 13:49:40 +0100

    Generic Property Editor improved

---
+++ razarion-client/src/main/java/com/btxtech/client/editor/generic/propertyeditors/BooleanEditor.java
@@ -0,0 +1,43 @@
+package com.btxtech.client.editor.generic.propertyeditors;
+
+import com.btxtech.shared.system.ExceptionHandler;
+import elemental2.dom.DomGlobal;
+import elemental2.dom.HTMLElement;
+import elemental2.dom.HTMLInputElement;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+@Dependent
+public class BooleanEditor extends AbstractPropertyEditor<Boolean> {
+    // private Logger logger = Logger.getLogger(BooleanEditor.class.getName());
+    @Inject
+    private ExceptionHandler exceptionHandler;
+    private HTMLInputElement htmlInputElement;
+
+    @PostConstruct
+    public void postConstruct() {
+        htmlInputElement = (HTMLInputElement) DomGlobal.document.createElement("input");
+        htmlInputElement.type = "checkbox";
+    }
+
+    @Override
+    public void showValue() {
+        htmlInputElement.checked = getPropertyValue(false);
+
+        htmlInputElement.addEventListener("input", event -> {
+            try {
+                setPropertyValue(htmlInputElement.checked);
+            } catch (Throwable t) {
+                exceptionHandler.handleException("Cannot set property value for property: " + getPropertyModel(), t);
+            }
+        }, false);
+    }
+
+
+    @Override
+    public HTMLElement getElement() {
+        return htmlInputElement;
+    }
+}
--- razarion-client/src/main/java/com/btxtech/client/editor/generic/propertyeditors/PropertyEditorClassFactory.java
+++ razarion-client/src/main/java/com/btxtech/client/editor/generic/propertyeditors/PropertyEditorClassFactory.java
@@ -6,7 +6,6 @@ import com.btxtech.shared.datatypes.Rectangle;
 import com.btxtech.shared.datatypes.Rectangle2D;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class PropertyEditorClassFactory {
@@ -16,11 +15,11 @@ public class PropertyEditorClassFactory {
         GENERIC_TYPES.put(String.class, StringEditor.class);
         GENERIC_TYPES.put(Integer.class, IntegerEditor.class);
         GENERIC_TYPES.put(Double.class, DoubleEditor.class);
+        GENERIC_TYPES.put(Boolean.class, BooleanEditor.class);
         GENERIC_TYPES.put(Rectangle.class, RectangleEditor.class);
         GENERIC_TYPES.put(Rectangle2D.class, Rectangle2DEditor.class);
         GENERIC_TYPES.put(DecimalPosition.class, null); // TODO
         GENERIC_TYPES.put(Index.class, null);// TODO
-        GENERIC_TYPES.put(List.class, ListEditor.class);
     }
 
     public static Class get(Class type) {
