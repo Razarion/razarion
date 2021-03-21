2484264e57ab28e39edaa300eabb80a1da0287b2
Beat <beat.keller@btxtech.com>
2021-03-21 23:19:28 +0100

    Gwt angular integration (generic property editor)

--- razarion-client/src/main/java/com/btxtech/client/editor/generic/AngularTreeNodeData.java
+++ razarion-client/src/main/java/com/btxtech/client/editor/generic/AngularTreeNodeData.java
@@ -3,10 +3,23 @@ package com.btxtech.client.editor.generic;
 import jsinterop.annotations.JsType;
 import jsinterop.base.Any;
 
+import java.util.logging.Logger;
+
 @JsType
 public class AngularTreeNodeData {
+    private static final Logger LOGGER = Logger.getLogger(AngularTreeNodeData.class.getName());
+
     public String name;
     public Any value;
     public boolean deleteAllowed;
     public boolean createAllowed;
+
+    public void onCreate() {
+        LOGGER.severe("onCreate");
+    }
+
+    public void onDelete() {
+        LOGGER.severe("onDelete");
+    }
+
 }
