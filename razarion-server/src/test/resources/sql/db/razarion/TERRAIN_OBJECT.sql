c9510bdb26b67432fc641b4e8dc4d78cd49d1806
Beat <beat.keller@btxtech.com>
2022-09-16 21:52:36 +0200

    Improve DB terrain

--- razarion-server/src/test/resources/sql/db/razarion/TERRAIN_OBJECT.sql
+++ razarion-server/src/test/resources/sql/db/razarion/TERRAIN_OBJECT.sql
@@ -12,20 +12,31 @@ CREATE TABLE IF NOT EXISTS `TERRAIN_OBJECT` (
   PRIMARY KEY (`id`),
   KEY `FKl6f1pat64mpk8p56h60kynr4s` (`threeJsModelPackConfig_id`),
   CONSTRAINT `FKl6f1pat64mpk8p56h60kynr4s` FOREIGN KEY (`threeJsModelPackConfig_id`) REFERENCES `THREE_JS_MODEL_PACK` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
 
 DELETE FROM `TERRAIN_OBJECT`;
 /*!40000 ALTER TABLE `TERRAIN_OBJECT` DISABLE KEYS */;
 INSERT INTO `TERRAIN_OBJECT` (`id`, `internalName`, `radius`, `threeJsModelPackConfig_id`) VALUES
 	(6, 'Palm Tree 1', 3, 9),
 	(7, 'Palm Tree 2', 3, 10),
-	(8, 'Stone pack 1', 3, 11),
+	(8, 'Stone 12', 3, 11),
 	(9, 'Fern 1', 3, 12),
 	(10, 'Fern 2', 3, 13),
 	(11, 'Leaves', 3, 14),
 	(12, 'Trunk 1', 3, 15),
 	(13, 'Palm', 3, 16),
-	(14, 'Trunk 3', 3, 17);
+	(14, 'Trunk 3', 3, 17),
+	(15, 'Stone 1', 3, 18),
+	(16, 'Stone 2', 3, 19),
+	(17, 'Stone 3', 3, 20),
+	(19, 'Stone 4', 3, 21),
+	(20, 'Stone 5', 3, 22),
+	(21, 'Stone 6', 3, 23),
+	(22, 'Stone 7', 3, 24),
+	(23, 'Stone 8', 3, 25),
+	(24, 'Stone 9', 3, 26),
+	(25, 'Stone 10', 3, 27),
+	(26, 'Stone 11', 3, 28);
 /*!40000 ALTER TABLE `TERRAIN_OBJECT` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
