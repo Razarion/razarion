c5453ed0fa0e013387d272b9b63f167546fd131e
HansRazarionDev <hans@btxtech.com>
2023-11-28 22:25:49 +0100

    Improve editor

---
+++ razarion-frontend/src/app/editor/common/acceleration/acceleration.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'acceleration',
+  templateUrl: './acceleration.component.html'
+})
+export class AccelerationComponent implements OnInit {
+  private readonly FINAL_SPEED_M_PER_S = 100 * 1000 / 3600; // Convert 100 km/h to m/s
+  @Input("acceleration")
+  acceleration: number | null = null;
+  @Output()
+  accelerationChange = new EventEmitter<number | null>();
+  seconds0To100?: number;
+
+  ngOnInit(): void {
+    if (this.acceleration || this.acceleration === 0) {
+      this.seconds0To100 = this.FINAL_SPEED_M_PER_S / this.acceleration;
+    } else {
+      this.seconds0To100 = undefined;
+    }
+  }
+
+  onChange(seconds0To100: number) {
+    this.acceleration = seconds0To100 ? (this.FINAL_SPEED_M_PER_S / seconds0To100) : null;
+    this.accelerationChange.emit(this.acceleration);
+  }
+}
